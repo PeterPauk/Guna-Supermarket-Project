ed2f2d3c457350caca6b93896e62b58769eefd40
z21ite17 <petkopauk@gmail.com>
2023-05-18 13:44:32 +0200

    slight improvements

--- guna0/Form1.Designer.cs
+++ guna0/Form1.Designer.cs
@@ -32,7 +32,11 @@ namespace guna0
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.gunaLabel1 = new Guna.UI.WinForms.GunaLabel();
             this.gunaGradientPanel1 = new Guna.UI.WinForms.GunaGradientPanel();
+            this.gunaPictureBox1 = new Guna.UI.WinForms.GunaPictureBox();
+            this.Username = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel2 = new Guna.UI.WinForms.GunaLabel();
             this.gunaGradientPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gunaPictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // gunaLabel1
@@ -42,12 +46,13 @@ namespace guna0
             this.gunaLabel1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.gunaLabel1.Font = new System.Drawing.Font("Microsoft PhagsPa", 35.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.gunaLabel1.ForeColor = System.Drawing.Color.White;
-            this.gunaLabel1.Location = new System.Drawing.Point(25, 2);
+            this.gunaLabel1.Location = new System.Drawing.Point(121, 13);
+            this.gunaLabel1.Margin = new System.Windows.Forms.Padding(0);
             this.gunaLabel1.Name = "gunaLabel1";
             this.gunaLabel1.Size = new System.Drawing.Size(172, 62);
             this.gunaLabel1.TabIndex = 0;
             this.gunaLabel1.Text = "LOGIN";
-            this.gunaLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.gunaLabel1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.gunaLabel1.Click += new System.EventHandler(this.gunaLabel1_Click);
             // 
             // gunaGradientPanel1
@@ -59,26 +64,67 @@ namespace guna0
             this.gunaGradientPanel1.GradientColor2 = System.Drawing.Color.LightGreen;
             this.gunaGradientPanel1.GradientColor3 = System.Drawing.Color.DarkKhaki;
             this.gunaGradientPanel1.GradientColor4 = System.Drawing.Color.Yellow;
-            this.gunaGradientPanel1.Location = new System.Drawing.Point(272, 7);
+            this.gunaGradientPanel1.Location = new System.Drawing.Point(-14, -18);
             this.gunaGradientPanel1.Name = "gunaGradientPanel1";
-            this.gunaGradientPanel1.Quality = 4;
-            this.gunaGradientPanel1.Size = new System.Drawing.Size(223, 72);
+            this.gunaGradientPanel1.Quality = 1;
+            this.gunaGradientPanel1.Size = new System.Drawing.Size(468, 75);
             this.gunaGradientPanel1.TabIndex = 1;
             this.gunaGradientPanel1.Text = "gunaGradientPanel1";
             this.gunaGradientPanel1.Click += new System.EventHandler(this.gunaGradientPanel1_Click);
+            // 
+            // gunaPictureBox1
+            // 
+            this.gunaPictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.gunaPictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("gunaPictureBox1.BackgroundImage")));
+            this.gunaPictureBox1.BaseColor = System.Drawing.Color.Silver;
+            this.gunaPictureBox1.Location = new System.Drawing.Point(397, -223);
+            this.gunaPictureBox1.Name = "gunaPictureBox1";
+            this.gunaPictureBox1.Size = new System.Drawing.Size(418, 715);
+            this.gunaPictureBox1.TabIndex = 2;
+            this.gunaPictureBox1.TabStop = false;
+            // 
+            // Username
+            // 
+            this.Username.AutoSize = true;
+            this.Username.Font = new System.Drawing.Font("Microsoft PhagsPa", 15F);
+            this.Username.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.Username.Location = new System.Drawing.Point(12, 99);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(115, 26);
+            this.Username.TabIndex = 3;
+            this.Username.Text = "USERNAME";
+            // 
+            // gunaLabel2
+            // 
+            this.gunaLabel2.AutoSize = true;
+            this.gunaLabel2.Font = new System.Drawing.Font("Microsoft PhagsPa", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.gunaLabel2.Location = new System.Drawing.Point(12, 213);
+            this.gunaLabel2.Name = "gunaLabel2";
+            this.gunaLabel2.Size = new System.Drawing.Size(118, 26);
+            this.gunaLabel2.TabIndex = 4;
+            this.gunaLabel2.Text = "PASSWORD";
+            this.gunaLabel2.Click += new System.EventHandler(this.gunaLabel2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 457);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 439);
+            this.Controls.Add(this.gunaLabel2);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.gunaPictureBox1);
             this.Controls.Add(this.gunaGradientPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.gunaGradientPanel1.ResumeLayout(false);
             this.gunaGradientPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gunaPictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -86,6 +132,9 @@ namespace guna0
 
         private Guna.UI.WinForms.GunaLabel gunaLabel1;
         private Guna.UI.WinForms.GunaGradientPanel gunaGradientPanel1;
+        private Guna.UI.WinForms.GunaPictureBox gunaPictureBox1;
+        private Guna.UI.WinForms.GunaLabel Username;
+        private Guna.UI.WinForms.GunaLabel gunaLabel2;
     }
 }
 
--- guna0/Form1.cs
+++ guna0/Form1.cs
@@ -31,5 +31,10 @@ namespace guna0
         {
 
         }
+
+        private void gunaLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
